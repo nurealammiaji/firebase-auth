fcca5fb7e60a32a97333228f77b3752ca264357e
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-23 20:42:02 +0600

    forgot password page modified

--- src/components/Forgot/Forgot.jsx
+++ src/components/Forgot/Forgot.jsx
@@ -4,6 +4,23 @@ const Forgot = () => {
     return (
         <div>
             <br /><br />
+            <div className="py-10 hero bg-base-200 rounded-xl">
+                <div className="hero-content">
+                    <div className="w-full max-w-sm shadow-2xl card bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="mt-2 form-control">
+                                <button type="submit" className="btn btn-primary">Send Reset Link</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
