b9bd84ccc969268929609a25f9d7cb3f3484f467
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-23 20:18:12 +0600

    home page modified

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
     return (
         <div>
             <br /><br />
-            <div className="rounded-2xl">
-                <h3>Home</h3>
+            <div className="hero-overlay bg-base-200 rounded-xl">
+                <div className="text-center hero-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <Link to="sign-up"><button className="btn btn-primary">Get Started</button></Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
