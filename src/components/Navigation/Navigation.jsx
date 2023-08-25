b9ee23149dfd6ab1f87fa8985cec0cf9ed2ac0ef
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-26 02:58:25 +0600

    context added to the navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProviders";
 
 
 const Navigation = () => {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <nav className="justify-between navbar bg-primary text-primary-content rounded-2xl">
             <div>
@@ -11,6 +16,9 @@ const Navigation = () => {
                 <NavLink className="text-lg normal-case btn btn-ghost" to="/">Home</NavLink>
                 <NavLink className="text-lg normal-case btn btn-ghost" to="/sign-up">Sign Up</NavLink>
                 <NavLink className="text-lg normal-case btn btn-ghost" to="/sign-in">Sign In</NavLink>
+            </div>
+            <div>
+                {user ? <><p>{user.email}</p><button className="px-4 py-3 mx-2 bg-white rounded-3xl">Sign Out</button></> : <><button>Sign In</button></>}
             </div>
         </nav>
     );
